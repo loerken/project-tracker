df871f712df243456d1b074c6bb62f50df9d3929
Horváth Károly <horvath.karoly04@gmail.com>
2026-02-17 18:49:58 +0100

    Created first API endpoint

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/api/tasks', (req, res) => {
+    const sql = "SELECT * FROM tasks ORDER BY created_at DESC";
+    
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running on the following address: http://localhost:${port}`);
 });
